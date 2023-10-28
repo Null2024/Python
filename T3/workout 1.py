be630b8c514b37fd5eec294dcdb8ef2009ff1149
Null2024 <117080456+Null2024@users.noreply.github.com>
2023-10-28 12:19:54 +0000

    Pending changes exported from your codespace

---
+++ T3/workout 1.py
@@ -0,0 +1,6 @@
+def person_inf(Name,Lname,Ag):
+    print(Name)
+    print(Lname)
+    print (Ag)
+
+person_inf("Mohammad","Ranjbar","20")
---
+++ T3/workout 2.py
@@ -0,0 +1,5 @@
+def Formula(T,V,Vr):
+    Dv_Dt = (-1/T)*(V-Vr)
+    print(Dv_Dt)
+    
+Formula(3,5,8)
